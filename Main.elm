f65d992cacbc098ba01effa84bfa3f9aa2473271
Noah Gordon <noahzacharygordon@gmail.com>
2016-11-06 16:27:52 -0500

    Refactor view into data

--- Main.elm
+++ Main.elm
@@ -1,17 +1,48 @@
+module Main exposing (..)
+
 import Svg
 import Svg.Attributes exposing (..)
 
-main = Svg.svg [ width "500", height "500" ] ([ player ] ++ enemies)
 
-player = Svg.circle [ r "20", fill "blue", transform "translate(250 250)" ] []
+main =
+    Svg.svg [ width "500", height "500" ] ([ player ] ++ enemies |> List.map drawCircle)
 
+
+player : Circle
+player =
+    { size = 40, color = "blue", position = ( 250, 250 ) }
+
+
+enemies : List Circle
 enemies =
-  [ Svg.circle [ r "10", fill "red", transform "translate(50, 100)" ] []
-  , Svg.circle [ r "20", fill "red", transform "translate(450, 450)" ] []
-  , Svg.circle [ r "20", fill "red", transform "translate(450, 100)" ] []
-  , Svg.circle [ r "30", fill "red", transform "translate(400, 250)" ] []
-  , Svg.circle [ r "30", fill "red", transform "translate(300, 50)" ] []
-  , Svg.circle [ r "40", fill "red", transform "translate(100, 250)" ] []
-  , Svg.circle [ r "40", fill "red", transform "translate(100, 250)" ] []
-  , Svg.circle [ r "50", fill "red", transform "translate(250, 400)" ] []
-  ]
+    [ { size = 20, color = "red", position = ( 50, 100 ) }
+    , { size = 40, color = "red", position = ( 450, 450 ) }
+    , { size = 40, color = "red", position = ( 450, 100 ) }
+    , { size = 60, color = "red", position = ( 400, 250 ) }
+    , { size = 60, color = "red", position = ( 300, 50 ) }
+    , { size = 80, color = "red", position = ( 100, 250 ) }
+    , { size = 90, color = "red", position = ( 100, 250 ) }
+    , { size = 100, color = "red", position = ( 250, 400 ) }
+    ]
+
+
+type alias Circle =
+    { size : Float
+    , color : String
+    , position : ( Float, Float )
+    }
+
+
+drawCircle : Circle -> Svg.Svg msg
+drawCircle circle =
+    let
+        radius =
+            circle.size / 2 |> toString
+
+        ( x, y ) =
+            circle.position
+
+        translation =
+            "translate(" ++ toString x ++ ", " ++ toString y ++ ")"
+    in
+        Svg.circle [ r radius, fill circle.color, transform translation ] []
