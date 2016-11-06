57e02f3e636bd851fe38b90d373d2b6cee899350
Noah Gordon <noahzacharygordon@gmail.com>
2016-11-06 16:10:03 -0500

    Draw enemy circles

--- Main.elm
+++ Main.elm
@@ -1,6 +1,17 @@
 import Svg
 import Svg.Attributes exposing (..)
 
-main = Svg.svg [] [ player ]
+main = Svg.svg [ width "500", height "500" ] ([ player ] ++ enemies)
 
-player = Svg.circle [ r "20", fill "blue", transform "translate(200 200)" ] []
+player = Svg.circle [ r "20", fill "blue", transform "translate(250 250)" ] []
+
+enemies =
+  [ Svg.circle [ r "10", fill "red", transform "translate(50, 100)" ] []
+  , Svg.circle [ r "20", fill "red", transform "translate(450, 450)" ] []
+  , Svg.circle [ r "20", fill "red", transform "translate(450, 100)" ] []
+  , Svg.circle [ r "30", fill "red", transform "translate(400, 250)" ] []
+  , Svg.circle [ r "30", fill "red", transform "translate(300, 50)" ] []
+  , Svg.circle [ r "40", fill "red", transform "translate(100, 250)" ] []
+  , Svg.circle [ r "40", fill "red", transform "translate(100, 250)" ] []
+  , Svg.circle [ r "50", fill "red", transform "translate(250, 400)" ] []
+  ]
