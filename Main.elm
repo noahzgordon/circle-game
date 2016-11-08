4a5b8f17bf5fca8c0fbe0581618179fe10e1545a
Noah Gordon <noahzacharygordon@gmail.com>
2016-11-08 18:16:33 -0500

    add width and height to svg

--- Main.elm
+++ Main.elm
@@ -1,6 +1,6 @@
 import Svg
 import Svg.Attributes exposing (..)
 
-main = Svg.svg [] [ player ]
+main = Svg.svg [ width "500", height "500" ] [ player ]
 
 player = Svg.circle [ r "20", fill "blue", transform "translate(200 200)" ] []
